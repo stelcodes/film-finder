62840bb91a620dc85c5c526b734f9a03f51a6201
Stel Abrego <stel@stel.codes>
2023-10-25 15:18:56 -0700

    checkpoint: download ics file

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/stelcodes/movie-cal
 
 go 1.20
+
+require (
+	github.com/arran4/golang-ical v0.1.0 // indirect
+	github.com/cavaliergopher/grab/v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,62 @@ package main
 
 import (
 	"fmt"
+	"github.com/cavaliergopher/grab/v3" // https://pkg.go.dev/github.com/cavaliergopher/grab/v3
+	"log"
+	"os" // https://pkg.go.dev/os
+	"time"
 )
 
+// Just using /tmp because that is simplest
+const CACHE_DIR = "/tmp/movie-cal-cache"
+
+func downloadFile(s string) (string, error) {
+	client := grab.NewClient()
+	req, _ := grab.NewRequest(CACHE_DIR, s)
+
+	// start download
+	fmt.Printf("Downloading %v...\n", req.URL())
+	resp := client.Do(req)
+	fmt.Printf("  %v\n", resp.HTTPResponse.Status)
+
+	// start UI loop
+	t := time.NewTicker(500 * time.Millisecond)
+	defer t.Stop()
+
+Loop:
+	for {
+		select {
+		case <-t.C:
+			fmt.Printf("  transferred %v / %v bytes (%.2f%%)\n",
+				resp.BytesComplete(),
+				resp.Size(),
+				100*resp.Progress())
+
+		case <-resp.Done:
+			// download is complete
+			break Loop
+		}
+	}
+
+	// check for errors
+	if err := resp.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Download failed: %v\n", err)
+		return "", err
+	}
+
+	fmt.Printf("Download saved to %v\n", resp.Filename)
+  return resp.Filename, nil
+}
+
 func main() {
-  fmt.Printf("hello world")
+	fmt.Printf("Starting movie-cal...\n")
+  err := os.MkdirAll(CACHE_DIR, 0750)
+	if err != nil {
+		log.Fatal(err)
+	}
+  _, err = downloadFile("https://cstpdx.com/schedule/list/?ical=1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
